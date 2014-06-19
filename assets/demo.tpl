a49fd23d7f45700560ae1a4619796f069aefc3e6
hechangmin <hechangmin@gmail.com>
2014-06-19 17:26:20 +0800

    add escapse output

--- assets/demo.tpl
+++ assets/demo.tpl
@@ -7,7 +7,7 @@
     <div id='content'>
         <ul>
         <% for (var i = 0, l = list.length; i < l; i ++) { %>
-            <li><%=list[i].index%>user: <%=list[i].user%> | site:<%=list[i].site%></li>
+            <li><%=list[i].index%>user: <%=list[i].user%> | site:<%-list[i].site%></li>
         <% } %>
         </ul>
     </div>
--- node_demo.js
+++ node_demo.js
@@ -8,8 +8,8 @@ var http = require('http'),
         site : '<a href="http://www.baidu.com" target="_blank">http://www.baidu.com</a>'
     },{
         index : 2,
-        user : 'qq',
-        site : 'http://www.qq.com'
+        user : '<span style="color:red;">qq</span>',
+        site : '<a href="http://www.qq.com" target="_blank">http://www.qq.com</a>'
     },{
         index : 3,
         user : 'ali',
