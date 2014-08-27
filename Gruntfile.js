5447dbed2affc57bda1627647f28b9077acebb6a
hechangmin <hechangmin@gmail.com>
2014-08-27 17:06:44 +0800

    add uglify

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                beautify: {
+                    ascii_only: true
+                }
+            },
+            build: {
+                src: 'lib/template.js',
+                dest: 'lib/template.min.js'
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.registerTask('default', ['uglify']);
+};
